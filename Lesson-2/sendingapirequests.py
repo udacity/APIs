55960cdd1e96d1356ad6ddc133c5e9eb229ae312
Lorenzo Brown <lorenzo@udacity.com>
2015-07-23 22:24:56 -0700

    Create sendingapirequests.py

---
+++ Lesson-2/sendingapirequests.py
@@ -0,0 +1,28 @@
+#THIS IS A WEBSERVER FOR DEMONSTRATING THE TYPES OF RESPONSES WE SEE FROM AN API ENDPOINT
+from flask import Flask
+app = Flask(__name__)
+
+#GET REQUEST
+
+@app.route('/getHello')
+def getRequestHello():
+	return "Hi, I got your GET Request!"
+
+#POST REQUEST
+@app.route('/postHello', methods = ['POST'])
+def postRequestHello():
+	return "I see you sent a POST message :-) "
+#UPDATE REQUEST
+@app.route('/updateHello', methods = ['UPDATE'])
+def updateRequestHello():
+	return "Sending Hello on an update request!"
+
+#DELETE REQUEST
+@app.route('/deleteHello', methods = ['DELETE'])
+def deleteRequestHello():
+	return "Deleting your hard drive.....haha just kidding! I received a DELETE request!"
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0', port=5000)	
+
