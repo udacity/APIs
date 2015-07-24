df8987cab713a37e34e2312963ff7751c7ee0852
Lorenzo Brown <lorenzo@udacity.com>
2015-07-23 22:35:50 -0700

    Create sendingARequestWithPython.py

---
+++ Lesson-2/sendingARequestWithPython.py
@@ -0,0 +1,17 @@
+def GetGeocodeLocation(inputString):
+	#Use Google Maps to convert a location into Latitute/Longitute coordinates
+	#FORMAT: https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=API_KEY
+    #Then Store it in the DB
+    
+
+    locationString = inputString.replace(" ", "+")
+    url = ('https://maps.googleapis.com/maps/api/geocode/json?address=%s&key=%s'% (locationString, google_api_key))
+    h = httplib2.Http()
+    result = json.loads(h.request(url,'GET')[1])
+    
+    latitude = result['results'][0]['geometry']['location']['lat']
+    longitude = result['results'][0]['geometry']['location']['lng']
+
+
+
+    return (latitude,longitude)
