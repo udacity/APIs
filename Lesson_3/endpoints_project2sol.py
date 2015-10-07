5025902d601beb483cdaeb5114072bd68fb4bdf3
Lorenzo Brown <lorenzo@udacity.com>
2015-10-06 19:10:07 -0700

    Create endpoints_project2sol.py

---
+++ Lesson_3/endpoints_project2sol.py
@@ -0,0 +1,44 @@
+from flask import Flask
+app = Flask(__name__)
+# Create the appropriate app.route functions, test and see if they work, and paste your URI’s in the boxes below.
+
+#Make an app.route() decorator here
+@app.route("/puppies", methods = ['GET', 'POST'])
+def puppiesFunction():
+  if request.method == 'GET':
+  	#Call the method to Get all of the puppies
+  	return getAllPuppies()
+  
+  elif request.method == 'POST':
+  	#Call the method to make a new puppy
+  	return makeANewPuppy()
+  
+ 
+#Make another app.route() decorator here that takes in an integer id in the 
+@app.route("/puppies/<int:id>", methods = ['GET', 'PUT', 'DELETE'])
+def puppiesFunction(id):
+  if request.method == 'GET':
+  	#Call the method to get a specific puppy based on their id
+  	return getPuppy(id)
+  if request.method == 'PUT':
+  	#Call the method to update a puppy
+  	return updatePuppy(id)
+  elif request.method == 'DELETE':
+  	#Call the method to remove a puppy
+  	return deletePuppy(id) 
+
+
+def getAllPuppies():
+  return "Getting All the puppies!"
+  
+def makeANewPuppy():
+  return "Creating A New Puppy!"
+
+def getPuppy(id):
+	return "Getting Puppy with id %s" % id
+	
+def updatePuppy(id):
+  return "Updating a Puppy with id %s" % id
+
+def deletePuppy(id):
+  return "Removing Puppy with id %s" % id
