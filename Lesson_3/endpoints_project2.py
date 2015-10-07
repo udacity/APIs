8c75e0081a81b0efe6fc46b41a89e765591d82e2
Lorenzo Brown <lorenzo@udacity.com>
2015-10-06 19:10:41 -0700

    Update endpoints_project2.py

--- Lesson_3/endpoints_project2.py
+++ Lesson_3/endpoints_project2.py
@@ -1,31 +1,44 @@
 from flask import Flask
 app = Flask(__name__)
-# Create the appropriate app.route functions, test and see if they work, and paste your URI’s in the boxes below.f
+# Create the appropriate app.route functions, test and see if they work, and paste your URI’s in the boxes below.
 
 #Make an app.route() decorator here
-def PuppiesFunction():
+
+def puppiesFunction():
   if request.method == 'GET':
-  #Call the method to Get all of the puppies
+  	#Call the method to Get all of the puppies
+  	
+  
   elif request.method == 'POST':
-  #Call the method to make a new puppy
+  	#Call the method to make a new puppy
+  
   
  
-#Make another app.route() decorator here that takes in an integer id in the URI
-def PuppiesFunction(id):
+#Make another app.route() decorator here that takes in an integer id in the 
+
+def puppiesFunction(id):
+  if request.method == 'GET':
+  	#Call the method to get a specific puppy based on their id
+  	
   if request.method == 'PUT':
-  #Call the method to update a puppy
+  	#Call the method to update a puppy
+  	
   elif request.method == 'DELETE':
-  #Call the method to remove a puppy 
+  	#Call the method to remove a puppy
+  	
 
 
-def GetAllPuppies():
+def getAllPuppies():
   return "Getting All the puppies!"
   
-def MakeANewPuppy():
+def makeANewPuppy():
   return "Creating A New Puppy!"
 
-def UpdatePuppy(id):
+def getPuppy(id):
+	return "Getting Puppy with id %s" % id
+	
+def updatePuppy(id):
   return "Updating a Puppy with id %s" % id
 
-def DeletePuppy(id):
+def deletePuppy(id):
   return "Removing Puppy with id %s" % id
