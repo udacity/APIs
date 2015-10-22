efb696d445b2557e40005e69ee62d5147ffb82a1
Adarsh Nair <adarsh108@gmail.com>
2015-10-22 13:39:24 -0700

    Update geocode.py

--- Lesson_2/10_Requesting from Python Code/geocode.py
+++ Lesson_2/10_Requesting from Python Code/geocode.py
@@ -10,6 +10,7 @@ def getGeocodeLocation(inputString):
     url = ('https://maps.googleapis.com/maps/api/geocode/json?address=%s&key=%s'% (locationString, google_api_key))
     h = httplib2.Http()
     result = json.loads(h.request(url,'GET')[1])
-    latitude = result['results'][0]['geometry']['location']['lat']
-    longitude = result['results'][0]['geometry']['location']['lng']
-    return (latitude,longitude)
+    # latitude = result['results'][0]['geometry']['location']['lat']
+    # longitude = result['results'][0]['geometry']['location']['lng']
+    # return (latitude,longitude)
+    return result
