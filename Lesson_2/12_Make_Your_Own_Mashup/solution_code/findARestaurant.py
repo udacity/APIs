f7be069bd904ab3275243557709350560838817f
Lorenzo Brown <lorenzo@udacity.com>
2015-11-02 16:43:04 -0800

    Update findARestaurant.py

--- Lesson_2/12_Make_Your_Own_Mashup/solution_code/findARestaurant.py
+++ Lesson_2/12_Make_Your_Own_Mashup/solution_code/findARestaurant.py
@@ -30,20 +30,19 @@ def findARestaurant(mealType,location):
 		for i in restaurant_address:
 			address += i + " "
 		restaurant_address = address
-		#Get a  300x300 picture of the restaurant using the venue_id (you can change this by altering the 300x300 value in the URL or replacing it with 'orginal' to get the original picture
+		#4.  Get a  300x300 picture of the restaurant using the venue_id (you can change this by altering the 300x300 value in the URL or replacing it with 'orginal' to get the original picture
 		url = ('https://api.foursquare.com/v2/venues/%s/photos?client_id=%s&v=20150603&client_secret=%s' % ((venue_id,foursquare_client_id,foursquare_client_secret)))
 		result = json.loads(h.request(url, 'GET')[1])
-		#Grab the first image
-		  #if no image available, insert default image url
-		#return a dictionary containing the restaurant name, address, and image url
+		#5.  Grab the first image
 		if result['response']['photos']['items']:
 			firstpic = result['response']['photos']['items'][0]
 			prefix = firstpic['prefix']
 			suffix = firstpic['suffix']
 			imageURL = prefix + "300x300" + suffix
 		else:
+			#6.  if no image available, insert default image url
 			imageURL = "http://pixabay.com/get/8926af5eb597ca51ca4c/1433440765/cheeseburger-34314_1280.png?direct"
-	
+		#7.  return a dictionary containing the restaurant name, address, and image url
 		restaurantInfo = [{'name':restaurant_name, 'address':restaurant_address, 'image':imageURL}]
 		print "Restaurant Name: %s" % restaurantInfo['name']
 		print "Restaurant Address: %s" % restaurantInfo['address']
