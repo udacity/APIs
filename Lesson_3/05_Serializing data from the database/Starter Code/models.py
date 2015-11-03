9f9c8a7a680ade81737ede0e5fcca002dd5a5ed5
Lorenzo Brown <lorenzo@udacity.com>
2015-11-02 17:20:36 -0800

    Update models.py

--- Lesson_3/05_Serializing data from the database/Starter Code/models.py
+++ Lesson_3/05_Serializing data from the database/Starter Code/models.py
@@ -11,16 +11,8 @@ class Puppy(Base):
     name =Column(String(80), nullable = False)
     id = Column(Integer, primary_key = True)
     description = Column(String(250))
+    #Add add a decorator property to serialize data from the database
 
-    @property
-    def serialize(self):
-       """Return object data in easily serializeable format"""
-       return {
-       		'id': self.id,
-           'name': self.name,
-           'description' : self.description
-       }
- 
 
 
 engine = create_engine('sqlite:///puppies.db')
