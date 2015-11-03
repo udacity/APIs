7c4131be683412b7b33f3d98e6cf39b87711a333
Lorenzo Brown <lorenzo@udacity.com>
2015-11-02 17:22:38 -0800

    Create models.py

---
+++ Lesson_3/05_Serializing data from the database/Solution Code/models.py
@@ -0,0 +1,27 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+class Puppy(Base):
+    __tablename__ = 'puppy'
+
+
+    name =Column(String(80), nullable = False)
+    id = Column(Integer, primary_key = True)
+    description = Column(String(250))
+
+    @property
+    def serialize(self):
+       """Return object data in easily serializeable format"""
+       return {
+       		'id': self.id,
+           'name': self.name,
+           'description' : self.description
+       }
+ 
+
+
+engine = create_engine('sqlite:///puppies.db')
+Base.metadata.create_all(engine)
