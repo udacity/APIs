93af5c888cda48feb9b151d3dcd7d399708c2765
Subhadeep Dey <SDey96@users.noreply.github.com>
2018-05-16 18:57:47 +0530

    Modify against PEP-8 style guidelines

--- Lesson_4/02_Adding Users and Logins/models.py
+++ Lesson_4/02_Adding Users and Logins/models.py
@@ -1,12 +1,15 @@
-from sqlalchemy import Column,Integer,String
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy import create_engine
 from passlib.apps import custom_app_context as pwd_context
 
 Base = declarative_base()
+
+
 class User(Base):
     __tablename__ = 'user'
+
     id = Column(Integer, primary_key=True)
     username = Column(String(32), index=True)
     password_hash = Column(String(64))
@@ -19,7 +22,5 @@ class User(Base):
 
 
 engine = create_engine('sqlite:///users.db')
- 
 
 Base.metadata.create_all(engine)
-    
